6ed7b592743354742581356c8603737229f18852
thislooksfun <thislooksfun@repbot.org>
2021-02-28 21:53:49 -0600

    test: add more test cases

--- Tests/UITests/ObservablePublishedTests.swift
+++ Tests/UITests/ObservablePublishedTests.swift
@@ -10,8 +10,18 @@ class ObservablePublishedTests: UITest {
         app.children(matching: .any).firstMatch.slowSwipeDown(offset: 0.5, distance: 0.1)
         let searchBar = app.navigationBars.firstMatch.children(matching: .searchField).firstMatch
         searchBar.tap()
-        searchBar.typeText("h")
 
+        searchBar.typeText("h")
         XCTAssertEqual(app.staticTexts["countLabel"].label, "Update count: 1")
+
+        searchBar.typeText("e")
+        XCTAssertEqual(app.staticTexts["countLabel"].label, "Update count: 2")
+
+        searchBar.typeText("llo")
+        XCTAssertEqual(app.staticTexts["countLabel"].label, "Update count: 5")
+
+        searchBar.typeText(XCUIKeyboardKey.delete.rawValue)
+        searchBar.typeText(XCUIKeyboardKey.delete.rawValue)
+        XCTAssertEqual(app.staticTexts["countLabel"].label, "Update count: 7")
     }
 }
